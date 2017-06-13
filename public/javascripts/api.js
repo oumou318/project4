2d8b66ed8f40d13bd7667c67b8c7ebc3445b6c46
Oumou318 <oumoudiallo318@gmail.com>
2017-06-13 12:14:36 -0400

    title of surah

---
+++ public/javascripts/api.js
@@ -0,0 +1,32 @@
+// useful parameters
+	/*1. format 'text' or 'audio'
+	2. language 'en', 'fr'
+	3. type ex. 'versebyverse', 'translation'
+
+
+http://api.alquran.cloud/edition?format=audio&language=en&type=versebyverse 
+
+data: 	{
+identifier: "ar.abdulbasitmurattal",
+language: "ar",
+name: "Abdul Basit",
+englishName: "Abdul Basit",
+format: "audio",
+type: "translation"
+},
+{
+identifier: "ar.abdullahbasfar",
+language: "ar",
+name: "Abdullah Basfar",
+englishName: "Abdullah Basfar",
+format: "audio",
+type: "versebyverse"
+},
+{
+identifier: "ar.abdurrahmaansudais",
+language: "ar",
+name: "Abdurrahmaan As-Sudais",
+englishName: "Abdurrahmaan As-Sudais",
+format: "audio",
+type: "versebyverse"
+},*/
